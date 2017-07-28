ad5a9b9ee39869f8be65375fc945f75ca1b3a554
Kant <krishan-kant@mercer.com>
2017-07-28 11:48:59 +0530

    adding test.cs

---
+++ Test/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test
+{
+    public class Test
+    {
+        public string getName()
+        {
+            return "Krishan";
+        }
+
+        public string getName1()
+        {
+            return "Nagendra";
+        }
+    }
+}
