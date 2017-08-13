12ac18d5d48bbbdc06b27a1d28d516f0039a8ba3
Kant <krishan-kant@mercer.com>
2017-08-13 19:02:51 +0530

    adding coverage check file

---
+++ Check_Coverage.ps1
@@ -0,0 +1,13 @@
+﻿$xmldata = [XML](Get-Content "TestResults\emma\coverage.xml")
+[STRING]$percent_method = $xmldata.report.data.all.coverage[1].value
+[int]$percent_method = $percent_method.Substring(0,2)
+if ($percent_method -le 70)
+{
+echo Method coverage is less than 70 Percent. Failing the build.
+EXIT 1
+}
+else 
+{
+echo Method coverage is more than 70 Percent. Passing the build.
+EXIT 0
+}
